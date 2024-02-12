6593d0f6bee0b2fffa6a76d39280da3f32b4fcdf
krKimMinSeo <neck3922@gmail.com>
2024-02-12 21:14:58 +0900

    Nonvol_cache add complete
    경로를 저장해둔 변수를 지우고 robocopy 명령어 안에 직접 절대경로를 입력하니 정상적으로 프로그램이 작동됨

--- LDDC.bat
+++ LDDC.bat
@@ -111,7 +111,16 @@ if not exist "%nonvol%" (
     mkdir "%nonvol%"
     echo Created %nonvol% directory.
 
-    
+    ::cache
+    if not exist "%cache%" (
+        mkdir "%cache%"
+        set "cache=%nonvol%\_cache"
+        :: set "chromeCache=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Cache"
+        echo start cache_part at Date: %DATE% Time: %TIME% >> _result\log.txt
+        robocopy "C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Cache" "%cache%" /s /e /z /copy:DAT /r:3 /w:5 /log:"%cache%\robocopy_chrome_cache.txt"
+    ) else (
+        echo %cache% directory already exists. passing...
+    )
 
 ) else (
     echo %nonvol% directory already exists. passing...
