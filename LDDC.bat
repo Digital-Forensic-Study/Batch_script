5089600865a9ddd9e0b81e2e3119cbc00b11b65a
krKimMinSeo <neck3922@gmail.com>
2024-02-12 17:20:08 +0900

    Add :cache
    비휘발성 데이터 수집으로 robocopy로 크롬 브라우저의 캐시 데이터를 하위 디렉토리와 모든 파일을 포함하여 복사하여 텍스쳐 형태로 저장하는 명령어 추가

--- LDDC.bat
+++ LDDC.bat
@@ -13,7 +13,8 @@ set "logonAccount=%vol%\_logonAccount"
 
 ::set "web=%nonvol%\_web"
 ::set "robocopy=%nonvol%\_robocopy"
-::set "cache=%nonvol%\_cache"
+set "cache=%nonvol%\_cache"
+set "chrome_cache=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Cache"
 
 :REDO
 echo ---------- Live Disk Data Collector ----------
@@ -97,6 +98,18 @@ if not exist "%vol%" (
 if not exist "%nonvol%" (
     mkdir "%nonvol%"
     echo Created %nonvol% directory.
+
+    ::cache
+    if not exist "%cache%" (
+        mkdir "%cache%"
+        set "cache=%nonvol%\_cache"
+        set "chrome_cache=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Cache"
+        echo start cache_part at Date: %DATE% Time: %TIME% >> _result\log.txt
+        robocopy "%chrome_cache%" "%cache%" /s /e /z /copy:DAT /r:3 /w:5 /log:"%cache%\robocopy_chrome_cache.txt"
+    ) else (
+        echo %cache% directory already exists. passing...
+    )
+
 ) else (
     echo %nonvol% directory already exists. passing...
 )
@@ -183,6 +196,18 @@ if not exist "%prefetch%" (
 if not exist "%nonvol%" (
     mkdir "%nonvol%"
     echo Created %nonvol% directory.
+    
+    ::cache
+    if not exist "%cache%" (
+        mkdir "%cache%"
+        set "cache=%nonvol%\_cache"
+        set "chrome_cache=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Cache"
+        echo start cache_part at Date: %DATE% Time: %TIME% >> _result\log.txt
+        robocopy "%chrome_cache%" "%cache%" /s /e /z /copy:DAT /r:3 /w:5 /log:"%cache%\robocopy_chrome_cache.txt"
+    ) else (
+        echo %cache% directory already exists. passing...
+    )
+
 ) else (
     echo %nonvol% directory already exists. passing...
 )
